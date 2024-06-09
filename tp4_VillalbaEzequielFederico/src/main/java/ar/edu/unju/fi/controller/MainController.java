ab6d84e19db7274f80dffc4713fbda204d668ed9
EzequielVillalba10 <ezequielvillagf@gmail.com>
2024-06-08 22:26:16 -0300

    Creacion de tabla para carreras en hoja html

--- tp4_VillalbaEzequielFederico/src/main/java/ar/edu/unju/fi/controller/MainController.java
+++ tp4_VillalbaEzequielFederico/src/main/java/ar/edu/unju/fi/controller/MainController.java
@@ -3,11 +3,14 @@ package ar.edu.unju.fi.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import ch.qos.logback.core.model.Model;
+
 @Controller
 public class MainController {
 	
-	@GetMapping
-	public String getIndexPage() {
+	@GetMapping("/inicio")
+	public String getIndexPage(org.springframework.ui.Model model) {
+		model.addAttribute("titulo", "inicio");
 		return "index";
 	}
 }
