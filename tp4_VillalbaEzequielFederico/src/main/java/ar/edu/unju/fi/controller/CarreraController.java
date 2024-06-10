8d6f4d9b032a3a98a6699dfc789b3c2ae54e1e63
EzequielVillalba10 <ezequielvillagf@gmail.com>
2024-06-09 21:15:56 -0300

    Correcion de errores en la pagina carrera html y agrego un getMapping.

--- tp4_VillalbaEzequielFederico/src/main/java/ar/edu/unju/fi/controller/CarreraController.java
+++ tp4_VillalbaEzequielFederico/src/main/java/ar/edu/unju/fi/controller/CarreraController.java
@@ -1,20 +1,38 @@
 package ar.edu.unju.fi.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ar.edu.unju.fi.collections.CollectionCarrera;
+import ar.edu.unju.fi.model.Carrera;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 @RequestMapping("/carrera")
 public class CarreraController {
-
+	@Autowired
+	private Carrera carrera;
+	
 	@GetMapping("/listado")
 	public String getCarrerasPage(Model model) {
 		model.addAttribute("carreras", CollectionCarrera.getCarreras());
 		model.addAttribute("titulo", "Carreras");
 		return "carreras";
 	}
+	
+	
+	@GetMapping("/nuevo")
+	public String getNuevaCarreraPage(Model model) {
+		boolean edicion = false;
+		model.addAttribute("carrera", carrera);
+		model.addAttribute("edicion", edicion);
+		model.addAttribute("titulo", "Nueva Carrera");
+		return "carrera";
+	}
+	
 }
